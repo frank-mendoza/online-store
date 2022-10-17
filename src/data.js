b38a13e353a34f1317bd6075bbd49393ed2ed552
frank <frnkmendoza101@gmail.com>
2022-10-17 10:18:42 +0800

    add git files

---
+++ src/data.js
@@ -0,0 +1,165 @@
+import sound from "./img/pexels-sound-on-3756879.png"
+import tranmautritam from "./img/pexels-tranmautritam-251225.png"
+import magda from "./img/pexels-magda-ehlers-1279813.png"
+import fauxels from "./img/pexels-fauxels-3183197.png"
+import thisisengineering from "./img/pexels-thisisengineering-3913025.png"
+import alexander from "./img/pexels-alexander-grey-1209843.png"
+import tianyi from "./img/tianyi-ma.png"
+import designecologist from "./img/designecologist.png"
+
+const data = [
+  {
+    src: sound,
+    alt: "pexels-sound-on-3756879",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 1
+  },
+  {
+    src: tranmautritam,
+    alt: "pexels-tranmautritam-251225",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 2
+  },
+  {
+    src: magda,
+    alt: "pexels-magda-ehlers-1279813",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 2
+  },
+  {
+    src: fauxels,
+    alt: "pexels-fauxels-3183197",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 3
+  },
+  {
+    src: thisisengineering,
+    alt: "pexels-thisisengineering-3913025",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 2
+  },
+  {
+    src: alexander,
+    alt: "pexels-alexander-grey-1209843",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 3
+  },
+  {
+    src: tianyi,
+    alt: "tianyi-ma",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 3
+  },
+  {
+    src: designecologist,
+    alt: "designecologist",
+    title: "Sample Website Title",
+    client: "Client Project",
+    type: 1
+  },
+]
+
+export default data
+
+export const about =[
+  {
+    id: 0,
+    name: 'adobe xd',
+    src: require('./img/adobexd.png')
+  },
+  {
+    id: 1,
+    name: 'bootstrap',
+    src: require('./img/bootstrap.png')
+  },
+  {
+    id: 2,
+    name: 'css',
+    src: require('./img/css.png')
+  },
+  {
+    id: 3,
+    name: 'figma',
+    src: require('./img/figma.png')
+  },
+  {
+    id: 4,
+    name: 'git',
+    src: require('./img/git.png')
+  },
+  {
+    id: 5,
+    name: 'html',
+    src: require('./img/html.png')
+  },
+  {
+    id: 6,
+    name: 'illustrator',
+    src: require('./img/illustrator.png')
+  },
+  {
+    id: 7,
+    name: 'js',
+    src: require('./img/js.png')
+  },
+  {
+    id: 8,
+    name: 'photoshop',
+    src: require('./img/photoshop.png')
+  },
+  {
+    id: 9,
+    name: 'pug',
+    src: require('./img/pug.png')
+  },
+  {
+    id: 10,
+    name: 'reactjs',
+    src: require('./img/reactjs.png')
+  },
+  {
+    id: 11,
+    name: 'reactnative',
+    src: require('./img/reactnative.png')
+  },
+  {
+    id: 12,
+    name: 'sass',
+    src: require('./img/sass.png')
+  },
+  {
+    id: 13,
+    name: 'wordpress',
+    src: require('./img/wordpress.jpg')
+  },
+]
+
+export const home = [
+  {
+    id: 0,
+    name: 'header-01',
+    src: require('./img/header-01.svg')
+  },
+  {
+    id: 1,
+    name: 'header-02',
+    src: require('./img/header-02.svg')
+  },
+  {
+    id: 2,
+    name: 'header-03',
+    src: require('./img/header-03.svg')
+  },
+  {
+    id: 3,
+    name: 'header-04',
+    src: require('./img/header-04.svg')
+  },
+]
